c9b9d7a8e8c56ec366f2374243475d3a0f9116e6
4nth0ny1 <acatullo4@gmail.com>
2022-01-26 17:51:20 -0600

    add notes from leetcode

--- bst.js
+++ bst.js
@@ -1,20 +1,37 @@
 // convert a sorted array to a binary search tree
+// #108 Leetcode 
+
+// for this one we will use recursion to traverse the array and pass the tests. 
 
 // o(n) time complexity - o(n) space complexity
 
 function TreeNode(val, left, right) {
     this.val = (val === undefined ? 0 : val);
-    this.left = (left === undefined ? null)
+    this.left = (left === undefined ? null : left);
+    this.right = (right === undefined ? null : right);
 }
 
-const arrayToBST = (nums, left = 0, right = nums.length - 1) => {
+// o(n) time complexity - o(n) space complexity
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+ var sortedArrayToBST = function(nums, left = 0, right = nums.length - 1) {
     if (left > right) return null;
     
     let mid = Math.floor((left + right) / 2);
     let root = new TreeNode(nums[mid]);
 
-    root.left = arrayToBST(nums, left, mid - 1);
-    root.right = arrayToBST(nums, mid + 1, right);
+    root.left = sortedArrayToBST(nums, left, mid - 1);
+    root.right = sortedArrayToBST(nums, mid + 1, right);
 
     return root;
 }
