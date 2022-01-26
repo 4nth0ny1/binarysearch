82d7d3f1ee15e93df18b7991b01a51a3f1af244b
4nth0ny1 <acatullo4@gmail.com>
2022-01-26 17:46:24 -0600

    add function but need to define Tree Node

--- bst.js
+++ bst.js
@@ -1,2 +1,22 @@
-const array = [-100, -50, 0, 50, 100];
+// convert a sorted array to a binary search tree
 
+// o(n) time complexity - o(n) space complexity
+
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val);
+    this.left = (left === undefined ? null)
+}
+
+const arrayToBST = (nums, left = 0, right = nums.length - 1) => {
+    if (left > right) return null;
+    
+    let mid = Math.floor((left + right) / 2);
+    let root = new TreeNode(nums[mid]);
+
+    root.left = arrayToBST(nums, left, mid - 1);
+    root.right = arrayToBST(nums, mid + 1, right);
+
+    return root;
+}
+const array = [-100, -50, 0, 50, 100];
+console.log(arrayToBST(array))
